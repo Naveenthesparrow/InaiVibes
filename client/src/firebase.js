56521ca90e57e3884d550ae3c4900d4d5d088c36
Naveenthesparrow <naveenneevan6380@gmail.com>
2025-02-06 12:16:33 +0530

    Authentication Done

---
+++ client/src/firebase.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBztqcK2hYSugyURyhOCOv-ENpptfMewiE",
+  authDomain: "inaivibe.firebaseapp.com",
+  projectId: "inaivibe",
+  storageBucket: "inaivibe.firebasestorage.app",
+  messagingSenderId: "821832600869",
+  appId: "1:821832600869:web:6bd194147b779ca4589ecc",
+  measurementId: "G-75S2DC4J7B"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
